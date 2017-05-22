248556a1a3e2d3b73fa4dd4a3fc59b01f3dbd8f7
Nic <fristurt@hotmail.com>
2017-05-22 11:24:04 +0200

    further rename

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 "use strict"
-const PoosyBot = require('./justanotherdiscordbot.js')
+const JustAnotherDiscordBot = require('./justanotherdiscordbot.js')
 const config = require('./config.js')
 const words = require('./words.js')
-const poosybotDb = require('./db_abstraction.js')
+const justanotherdiscordbotDb = require('./db_abstraction.js')
 const Discord = require('discord.js')
 const client = new Discord.Client();
-const poosybot = new PoosyBot(client, poosybotDb(), config, words);
+const poosybot = new JustAnotherDiscordBot(client, justanotherdiscordbotDb(), config, words);
 
 process.on('unhandledRejection', function (err) {
     throw err;
